a85d9893c8fa32047f976e71e3dd068688a53a19
Abduraimbek Yarkinov <yarkinovabduraimbek@gmail.com>
2025-08-07 13:58:18 +0500

    Initial project setup for travel_invest app
    Add initial Flutter project structure with Android and iOS platform support, Dart analysis options, localization assets, and basic configuration files. Includes .gitignore, README, and starter source files for app entry points.

---
+++ android/app/src/main/kotlin/com/example/travel_invest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travel_invest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'app/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  EasyLocalization.logger.enableLevels = [];
+  await EasyLocalization.ensureInitialized();
+  await initializeDateFormatting('uz');
+
+  runApp(
+    ProviderScope(
+      child: EasyLocalization(
+        path: 'assets/translations',
+        startLocale: Locale('uz'),
+        supportedLocales: [Locale('uz')],
+        child: App(),
+      ),
+    ),
+  );
+}
